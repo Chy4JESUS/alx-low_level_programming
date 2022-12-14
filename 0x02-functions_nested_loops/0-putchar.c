15587bcc550f92ebdc28bd26574f9be8e58af6e1
Chy4JESUS <117743005+Chy4JESUS@users.noreply.github.com>
2022-12-14 11:45:39 -0800

    Add files via upload
    Nested loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
 #include "main.h"
+
 /**
- *  main - print _putchar on a new line
- *  Return; 0
+ * main - Entry point
+ * Return:0
  */
+
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (o);
+char c[9] = "_putchar";
+int i = 0;
+while (i < 8)
+{
+_putchar(c[i]);
+i++;
 }
-
+_putchar('\n');
+return (0);
+}
