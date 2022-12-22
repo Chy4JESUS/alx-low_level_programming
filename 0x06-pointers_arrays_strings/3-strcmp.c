ece5ef79caf62334c5ee3686a11808fb60eb42b5
Chy4JESUS <117743005+Chy4JESUS@users.noreply.github.com>
2022-12-22 23:12:04 +0100

    Add files via upload
    string array

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,20 +1,26 @@
 #include "main.h"
-
 /**
  * _strcmp - compares two strings
- * @s1: string one
- * @s2: string two
- * Return: 0 is strings are equal else, defference between strings
- */
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 0 if matching, and ns1 - ns2 if not matching
+*/
+
 int _strcmp(char *s1, char *s2)
 {
-	for (; *s1 != '\0' && *s2 != '\0'; s2++)
-		if (*s1 != *s2)
-			return (*s1 - *s2);
-	if (*s1 != '\0')
-		return (*s1);
-	else if (*s2 != '\0')
-		return (*s2);
+	int i;
 
+	/**
+	 * We have reached the end of S1 OR we have reached the end of S2
+	*/
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
 	return (0);
 }
+
